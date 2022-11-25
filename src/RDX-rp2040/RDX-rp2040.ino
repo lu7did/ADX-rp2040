cfc243484a27ae279924a306080dde31a81475d9
lu7did <31574049+lu7did@users.noreply.github.com>
2022-11-25 10:57:50 -0300

    RDX

--- src/RDX-rp2040/RDX-rp2040.ino
+++ src/RDX-rp2040/RDX-rp2040.ino
@@ -93,6 +93,25 @@
 */
 
 #include "RDX-rp2040.h"
+#include "crc.h"
+#include "constants.h"
+#include "pack.h"
+#include "encode.h"
+#include "decode_ft8.h"
+#include "gen_ft8.h"
+
+#include "tx_ft8.h"
+#include "rx_ft8.h"
+
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+#include "hardware/adc.h"
+#include "hardware/dma.h"
+
+#include "pico/multicore.h"
+#include "hardware/irq.h"
 
 /*-------------------------------------------------
    IDENTIFICATION DIVISION.
@@ -117,6 +136,24 @@ struct tm timeinfo;        //current time
 struct tm timeprev;        //epoch time
 time_t t_ofs = 0;          //time correction after sync (0 if not sync-ed)
 bool stopCore1 = true;
+
+/*-------------------------------------------------------
+ * ft8 definitions
+ */
+ 
+message_info CurrentStation;
+UserSendSelection sendChoices;
+message_info message_list[kMax_decoded_messages]; // probably needs to be memset cleared before each decode
+
+
+bool send = false;
+bool justSent = false; //must recieve right after sending
+bool autosend = true;
+bool cq_only = false;
+uint64_t config_us;
+uint64_t fine_offset_us = 0; //in us
+int16_t signal_for_processing[num_samples_processed] = {0};
+uint32_t handler_max_time = 0;
 
 //*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
 //*                             Global Variables                                             *
@@ -144,6 +181,318 @@ int TXSW_State;
 int Bdly = 250;
 
 Si5351 si5351;
+//*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+//*  core1 processing                                                                        *
+//*  irq handler to process samples in the background using core1                            *
+//*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+void core1_irq_handler()
+{
+
+/*-------------------------------------
+ * 
+ * Copy capture_buf to signal_for_processing array, take fft and save to power, check for user keypad input
+ */
+    while (rp2040.fifo.available())
+    
+    {   
+        uint32_t handler_start = time_us_32();
+        uint32_t fdx = rp2040.fifo.pop();
+        uint16_t idx = (uint16_t) fdx;
+
+/*-------
+ * extract signal for processing
+ */
+        for(int i = 0; i < nfft; i++){
+            fresh_signal[i] -= DC_BIAS;
+        }
+        inc_extract_power(fresh_signal);
+/*-------
+ * processing shouldn't last more than 160 mSecs
+ */
+        uint32_t handler_time = (time_us_32() - handler_start)/1000;
+        if (handler_time > handler_max_time){
+            handler_max_time = handler_time;
+        }
+    }
+    multicore_fifo_clear_irq(); // Clear interrupt
+}
+
+/*-------------------------------
+ * setup1()
+ * core1 is held from processing till released from the main setup() when ready to go
+ */
+void setup1(void)
+
+{
+/*--------------------  
+ * wait till cleared
+ */
+    uint32_t tc1 = time_us_32();
+    while (stopCore1) {
+      while (time_us_32()-tc1 < 1000) {}
+      tc1=time_us_32();
+    }
+
+ /*---------   
+  * configure core1 interrupt handler
+  */
+    printf("second core running!\n");
+    multicore_fifo_clear_irq();
+    irq_set_exclusive_handler(SIO_IRQ_PROC1, core1_irq_handler);
+    irq_set_enabled(SIO_IRQ_PROC1, true);
+
+/*----------
+ * wait forever, actual processing is made at the interrupt handler
+ * the delay is just to avoid a smart-ass compiler optimization to 
+ * remove the infinite loop altogether
+ */
+    while (true)
+    {
+        delay(1000);
+    }
+}
+/*-----------------
+ * setup ft8 operation
+ */
+//*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+//*  ft8 processing                                                                          *
+//*  setup and handle the ft8 processing, this is actually the orchestation of the ft8lib    *
+//*  functions by Karliss Goba                                                               *
+//*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+ void setup_ft8() {
+
+/*--------
+ * the original overclock the processor, the initial version of this
+ * firmware won't
+ */
+    //overclocking the processor
+    //133MHz default, 250MHz is safe at 1.1V and for flash
+    //if using clock > 290MHz, increase voltage and add flash divider
+    //see https://raspberrypi.github.io/pico-sdk-doxygen/vreg_8h.html
+
+    //vreg_set_voltage(VREG_VOLTAGE_DEFAULT);
+    //@@@ set_sys_clock_khz(250000,true);
+
+/*------
+ * setup the ADC processing
+ */
+    setup_adc();
+
+/*------
+ * make the Hanning window for fft work
+ */
+    make_window();
+    
+
+/*------    
+ * freed the semaphore which where core1 is being held, so start it
+ */
+    stopCore1=false;
+
+/*------
+ * wait to settle
+ */
+
+    sleep_ms(1000);   
+    
+    
+} //end of the ft8 setup
+
+void ft8_run() {
+    
+        char message[25] = {0};
+        uint8_t tones[79] = {0};
+
+        if (send)
+        {
+            if (autosend)
+            {
+                printf("autosending\n");
+                printf("still here? %s\n", CurrentStation.station_callsign);
+                auto_gen_message(message, CurrentStation, MY_CALLSIGN, MY_GRID);
+            }
+            else
+            {
+                manual_gen_message(message, CurrentStation, sendChoices, MY_CALLSIGN, MY_GRID);
+                //reset send choices
+                sendChoices.call_cq = sendChoices.send_73 = sendChoices.send_grid = sendChoices.send_RR73 = sendChoices.send_RRR = sendChoices.send_Rsnr = sendChoices.send_snr = false;
+            }
+
+            printf("message to be sent: %s\n", message);
+            generate_ft8(message, tones);
+            //display message in my messages in yellow
+            //add_to_my_messages(message, 0xFFE0);
+
+            printf("SENDING FOR 12.64 SECONDS\n\n");
+            //gpio_put(LED_PIN, 1);
+            // ---> send_ft8(tones, rf_freq, CurrentStation.af_frequency);
+            /*-----[ADX-rp2004-plus]--------
+             * integration fix with ADX, freq held the base frequency
+             */
+            send_ft8(tones, freq, CurrentStation.af_frequency);
+            //gpio_put(LED_PIN, 0);
+            send = false;
+            justSent = true;
+        }
+
+        else
+        {
+            printf("RECIEVING FOR 12.8 SECONDS\n\n");
+
+            inc_collect_power();
+
+            printf("Handler max time: %d\n", handler_max_time);
+        
+            uint32_t decode_begin = time_us_32();
+            uint8_t num_decoded = decode_ft8(message_list);
+            printf("decoding took this long: %d us\n", time_us_32() - decode_begin);
+
+            decode_begin = time_us_32(); //i'll just reuse this variable
+
+            //testing example
+/*             message_list[0].self_rx_snr = -23;
+            message_list[0].time_offset = -0.1;
+            message_list[0].af_frequency = 2222;
+            strcat(message_list[0].full_text, "AA1GD KI5KGU R+04"); */
+            
+            //message_list[0].addressed_to_me = true;
+
+            identify_message_types(message_list, MY_CALLSIGN);
+            printf("identification took this long: %d us\n", time_us_32() - decode_begin);
+            printf("finished identifying message types\n");
+            justSent = false;
+
+            //reset_progress_bar(); 
+            //update_current_time(&current_hms, &config_hms, config_us);
+            //update_time_display(&current_hms);
+
+           // update_main_messages(message_list, num_decoded, cq_only, &current_hms);
+
+            //update_main_messages(message_list,1,false,&current_hms);
+        }
+
+        int selected_station = -1; //default is no response
+        int rTB = -1;              //rTB stands for responseTypeBuffer
+
+        printf("\nWAITING 2 SECONDS FOR USER INPUT\n");
+        printf("Enter station to respond to: ");
+
+        if (autosend){
+            for (int i = 0; i < kMax_decoded_messages; i++){
+                if (message_list[i].addressed_to_me){
+                    selected_station = i; //if autosending, finds the first occurence of callsign and automatically responds
+                    printf("autoselected to %d\n", selected_station);
+                    send = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("Enter response type: 0 for CQ, 1 for grid, 2 for snr, 3 for Rsnr, 4 for RRR, 5 for RR73, 6 for 73, 7+ for no choice:\n");
+        }
+
+        //while modulo greater than 12.8, OR aborted
+        
+        //@@@@ uint64_t overtime = (time_us_64() - config_us + config_hms.sec * 1000000) % 15000000;
+        //@@@@ here it needs to be sync with 15 secs edge
+        //This is to react to input       
+ /*       
+        uint64_t overtime=0;
+        if (overtime < 12800000){
+            printf("no time to select\n");
+            sleep_us(13000000 - overtime);
+        }
+        while ((time_us_64() - config_us + config_hms.sec * 1000000) % 15000000 > 12800000)
+        {
+            //scan for keypad input... otherwise do nothing
+            
+            
+            char char_selection = pico_keypad_get_key();
+
+            if (char_selection == '*')
+            { //make this the abort/menu key
+                //goto back to setup
+                goto reconfigure;
+            }
+            
+            
+            if (justSent)
+            {
+                continue;
+            }
+
+            if (!send || autosend)
+            {
+                selected_station = char_to_int(char_selection);
+            }
+            else if (!autosend)
+            {
+                rTB = char_to_int(char_selection);
+            }
+
+            if (selected_station < 0)
+            {
+                send = false;
+            }
+            else if (selected_station < kMax_decoded_messages)
+            {
+                send = true;
+            }
+
+        } //end of while loop
+*/
+        
+/*
+        if (!autosend)
+        {
+            if (rTB == 0)
+            {
+                sendChoices.call_cq = true;
+            }
+            else if (rTB == 1)
+            {
+                sendChoices.send_grid = true;
+            }
+            else if (rTB == 2)
+            {
+                sendChoices.send_snr = true;
+            }
+            else if (rTB == 3)
+            {
+                sendChoices.send_Rsnr = true;
+            }
+            else if (rTB == 4)
+            {
+                sendChoices.send_RRR = true;
+            }
+            else if (rTB == 5)
+            {
+                sendChoices.send_RR73 = true;
+            }
+            else if (rTB == 6)
+            {
+                sendChoices.send_73 = true;
+            }
+        }
+*/
+/*       
+        //need to make autosend automatically select the station
+        if (send)
+        {
+            bool same_station = strcmp(CurrentStation.station_callsign,message_list[selected_station].station_callsign);
+            CurrentStation = message_list[selected_station];
+            //update_current_station_display(CurrentStation, same_station);
+            printf("CurrentStation set to %d\n",selected_station);
+            printf("can callsign be seen? %s\n", CurrentStation.station_callsign);
+        }
+*/
+        //reset message_list
+        printf("clearing %d bytes of message_list\n", sizeof(message_list));
+        memset(message_list, 0, sizeof(message_list));
+        printf("done a loop!\n");
+        return;
+}        
 
 //*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
 //*                             setup() (core0)                                              *
@@ -197,6 +546,7 @@ void setup()
   _INFOLIST("%s finalized Ok\n", __func__);
 
 }
+
 //**************************[ END OF SETUP FUNCTION ]************************
 
 //***************************[ Main LOOP Function ]**************************
