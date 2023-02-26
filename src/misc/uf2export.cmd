e68d866466c5f992cb62af9d43a402be0106f567
lu7did <31574049+lu7did@users.noreply.github.com>
2023-02-26 12:00:40 -0300

    ADX-RDX-split

--- src/misc/uf2export.cmd
+++
@@ -1,3 +0,0 @@
-@echo "Copying UF2 export"
-@copy %1\*.uf2 Y:\Documents\GitHub\ADX-rp2040\bin\rdx-rp2040_v2.0.uf2
-@echo "Export completed"
--- src/test/turboRDXB/tx_ft8.h
+++
@@ -1,3 +0,0 @@
-#include <stdint.h>
-
-void send_ft8(uint8_t tones[], uint32_t RF_freq, uint16_t AF_freq);
